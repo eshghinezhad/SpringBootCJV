f01bb61cfbc521fcfacadf17f4c313d2ce7064e6
Monireh <monireheshghinezhad@gmail.com>
2025-07-25 13:29:55 -0400

    Refactor MovieController to return ResponseEntity for all endpoints;

--- src/main/java/Seneca/CJV/MovieListingBackend/controller/MovieController.java
+++ src/main/java/Seneca/CJV/MovieListingBackend/controller/MovieController.java
@@ -2,6 +2,7 @@ package Seneca.CJV.MovieListingBackend.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,47 +25,49 @@ public class MovieController {
     }
     // 1- create movies/tv shows to be added to the database
     @PostMapping
-    public Movie createMovie(@RequestBody Movie movie) {
+    public  Movie createMovie(@RequestBody Movie movie) {
         return movieService.createMovie(movie);
     }
     //  retrieves all the movies and tv shows in the database
     @GetMapping
-    public List<Movie> getAllMoviesTvs() {
-        return movieService.getAllMoviesTvs();
+    public ResponseEntity<List<Movie>> getAllMoviesTvs() {
+        List<Movie> movies = movieService.getAllMoviesTvs();
+        return ResponseEntity.ok(movies != null ? movies : List.of());
     }
     // 2- retrieves all the movies in the database
     @GetMapping("/movie")
-    public List<Movie> getAllMovies() {
-        return movieService.getAllMovies();
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return ResponseEntity.ok(movieService.getAllMovies());
     }
 
     // 3- retrieves all the tv shows in the database
     @GetMapping("/tv")
-    public List<Movie> getAllTvShows() {
-        return movieService.getAllTvShows();
+    public ResponseEntity<List<Movie>> getAllTvShows() {
+        return ResponseEntity.ok(movieService.getAllTvShows());
     }
     
     // 4- retrieves a list of movies and/or tv shows that contains the title
     //    specified in the request parameter 
     @GetMapping("/search")
-    public List<Movie> getMovieByTitle(@RequestParam String title) {
-        return movieService.getMovieByTitle(title);
+    public ResponseEntity<List<Movie>> getMovieByTitle(@RequestParam String title) {
+        List<Movie> movies = movieService.getMovieByTitle(title);
+        return ResponseEntity.ok(movies != null ? movies : List.of());
     }
 
     // 5-6- retrieves a list of featured movies or tv shows based on the type
     //    specified in the request parameter
     @GetMapping("/featured")
-    public List<Movie> getFeatured(@RequestParam  String type) {
-        return movieService.getFeatured(type);
+    public ResponseEntity<List<Movie>> getFeatured(@RequestParam  String type) {
+        return ResponseEntity.ok(movieService.getFeatured(type));
     }
 
     // 7- retrieve a specific movie or tv show by ID
     @GetMapping("/{id}")
-    public Movie getMovieById(@PathVariable String id) {
+    public ResponseEntity<Movie> getMovieById(@PathVariable String id) {
         Movie movie = movieService.getMovieById(id);
         if (movie == null) {
             throw new IllegalArgumentException("Invalid movie ID: " + id);
         }
-        return movie;
+        return ResponseEntity.ok(movie);
     }
 }
